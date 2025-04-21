d2d528b31c20ce1dd914ec63569198a90a614ce1
meow <62083650+uy-seng@users.noreply.github.com>
2025-04-21 18:38:52 -0500

    feature: update create and load sql (#11)

--- load.sql
+++ load.sql
@@ -1,9 +1,14 @@
--- Load data into Customer table
--- Assuming customer.csv contains: first_name,last_name,email,SSN,password
-LOAD DATA INFILE '/var/lib/mysql-files/data/customer.csv' INTO TABLE Customer FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS (first_name, last_name, email, SSN, password);
+-- 1. Customer
+LOAD DATA INFILE '/var/lib/mysql-files/data/customer.csv' INTO TABLE Customer FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS (
+    customer_id,
+    first_name,
+    last_name,
+    email,
+    SSN,
+    password
+);
 
--- -- Load data into Administrator table
--- -- Assuming administrator.csv contains: customer_id,first_name,last_name,email,SSN,password
+-- 2. Administrator
 LOAD DATA INFILE '/var/lib/mysql-files/data/administrator.csv' INTO TABLE Administrator FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS (
     administrator_id,
     first_name,
@@ -13,18 +18,18 @@ LOAD DATA INFILE '/var/lib/mysql-files/data/administrator.csv' INTO TABLE Admini
     password
 );
 
--- -- Load data into Account table
--- -- Assuming account.csv contains: owner_id,account_status,balance,account_history
+-- 3. Account
 LOAD DATA INFILE '/var/lib/mysql-files/data/account.csv' INTO TABLE Account FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS (
+    account_id,
     owner_id,
     account_status,
     balance,
     account_history
 );
 
--- -- Load data into Transaction table
--- -- Assuming transaction.csv contains: account_id,name,date,amount,new_balance,transaction_type
-LOAD DATA INFILE '/var/lib/mysql-files/data/transaction.csv' INTO TABLE Transaction FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS (
+-- 4. Transaction
+LOAD DATA INFILE '/var/lib/mysql-files/data/transaction.csv' INTO TABLE `Transaction` FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS (
+    transaction_id,
     account_id,
     name,
     `date`,
@@ -33,20 +38,10 @@ LOAD DATA INFILE '/var/lib/mysql-files/data/transaction.csv' INTO TABLE Transact
     transaction_type
 );
 
--- -- Load data into Bank_Withdrawal table
--- -- Assuming bank_withdrawal.csv contains: account_id,name,date,amount,new_balance,transaction_type
-LOAD DATA INFILE '/var/lib/mysql-files/data/bank_withdrawal.csv' INTO TABLE Bank_Withdrawal FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS (
-    account_id,
-    name,
-    `date`,
-    amount,
-    new_balance,
-    transaction_type
-);
-
--- -- Load data into Bank_Deposit table
--- -- Assuming bank_deposit.csv contains: account_id,name,date,amount,new_balance,transaction_type
+-- 5. Bank_Deposit
 LOAD DATA INFILE '/var/lib/mysql-files/data/bank_deposit.csv' INTO TABLE Bank_Deposit FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS (
+    deposit_id,
+    transaction_id,
     account_id,
     name,
     `date`,
@@ -55,9 +50,22 @@ LOAD DATA INFILE '/var/lib/mysql-files/data/bank_deposit.csv' INTO TABLE Bank_De
     transaction_type
 );
 
--- -- Load data into Bank_Transfer table
--- -- Assuming bank_transfer.csv contains: account_id,name,date,amount,new_balance,transaction_type,from_account,to_account
+-- 6. Bank_Withdrawal
+LOAD DATA INFILE '/var/lib/mysql-files/data/bank_withdrawal.csv' INTO TABLE Bank_Withdrawal FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS (
+    withdrawal_id,
+    transaction_id,
+    account_id,
+    name,
+    `date`,
+    amount,
+    new_balance,
+    transaction_type
+);
+
+-- 7. Bank_Transfer
 LOAD DATA INFILE '/var/lib/mysql-files/data/bank_transfer.csv' INTO TABLE Bank_Transfer FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS (
+    transfer_id,
+    transaction_id,
     account_id,
     name,
     `date`,
