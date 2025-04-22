fceea090cc68b7976c86f648f1b15e9ef2aa8b42
nova_44056 <senguy44056@gmail.com>
2025-04-21 19:23:03 -0500

    feature: add email check in admin registration

--- administrator_registration.php
+++ administrator_registration.php
@@ -15,10 +15,8 @@
     $SSN = $_POST["SSN"];
     $password = $_POST["password"];
     $passwordRepeat = $_POST["password_repeat"];
-
     $passwordHash = password_hash($password, PASSWORD_DEFAULT);
     $errors = array();
-
     if (empty($firstName) or empty($lastName) or empty($email) or empty($SSN) or empty($password)) {
       array_push($errors, "All fields are required");
     }
@@ -34,13 +32,25 @@
     if ($password !== $passwordRepeat) {
       array_push($errors, "Passwords do not match.");
     }
-
     if (count($errors) > 0) {
       foreach ($errors as $error) {
         echo "<div>$error</div>";
       }
     } else {
       require_once "database.php";
+      // check if email exist in administrator table
+      $checkSql = "SELECT email FROM Administrator WHERE email = ?";
+      $chkStmt = mysqli_prepare($conn, $checkSql);
+      mysqli_stmt_bind_param($chkStmt, "s", $email);
+      mysqli_stmt_execute($chkStmt);
+      mysqli_stmt_store_result($chkStmt);
+
+      // if email exist we will not allow registration
+      if (mysqli_stmt_num_rows($chkStmt) > 0) {
+        echo "<div>This email is already registered.</div>";
+        exit;
+      }
+      mysqli_stmt_close($chkStmt);
       $sql = "INSERT INTO Administrator (first_name, last_name, email, SSN, password) VALUES (?,?,?,?,?)";
       $stmt = mysqli_stmt_init($conn);
       $prepareStmt = mysqli_stmt_prepare($stmt, $sql);
@@ -54,10 +64,8 @@
     }
   }
   ?>
-
   <h1>Administrator Registration Form</h1>
   <form action="administrator_registration.php" method="post">
-    <!-- Note: administrator_id & customer_id is auto-generated so it isn’t needed as input -->
     <label for="first_name">First Name:</label>
     <input type="text" id="first_name" name="first_name" />
     <br /><br />
@@ -73,8 +81,8 @@
     <label for="password">Password:</label>
     <input type="password" id="password" name="password" />
     <br /><br />
-    <label for="password">Repeat Password:</label>
-    <input type="password" id="password" name="password_repeat">
+    <label for="password_repeat">Repeat Password:</label>
+    <input type="password" id="password_repeat" name="password_repeat" />
     <br><br>
     <input type="submit" value="Register" name="submit">
   </form>
